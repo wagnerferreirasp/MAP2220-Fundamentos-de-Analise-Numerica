416fea10b685fafb0acc24bf4dc54f431379982d
Wagner Ferreira Alves <wagnerferreirasp@hotmail.com>
2015-08-12 16:53:20 -0300

    Create analiseNumerica2.c

---
+++ analiseNumerica2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main()
+{
+    char *buf;
+
+    rl_bind_key('\t',rl_abort);//disable auto-complete
+
+    while((buf = readline("\n >> "))!=NULL)
+    {
+        if (strcmp(buf,"quit")==0)
+            break;
+
+        printf("[%s]\n",buf);
+
+        if (buf[0]!=0)
+            add_history(buf);
+    }
+
+    free(buf);
+
+    return 0;
+}
