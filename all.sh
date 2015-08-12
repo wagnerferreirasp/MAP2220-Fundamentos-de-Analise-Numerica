6965fc9cd79081c327946bd08472574d8c90841e
Wagner Ferreira Alves <wagnerferreirasp@hotmail.com>
2015-08-12 17:24:26 -0300

    Create all.sh

---
+++ all.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/wagnerferreirasp/MAP2220-Fundamentos-de-Analise-Numerica/master/analiseNumerica2.c -O analiseNumerica2.c
+gcc analiseNumerica2.c -o so1 -lreadline
+chmod +x so1
+./so1
