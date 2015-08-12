02011f8caac5629b0369e28e881090ecb3c5c06e
Wagner Ferreira Alves <wagnerferreirasp@hotmail.com>
2015-08-12 17:18:30 -0300

    Update analiseNumerica2.c

--- analiseNumerica2.c
+++ analiseNumerica2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h> /* for fork */
+#include <sys/types.h> /* for pid_t */
+#include <sys/wait.h> /* for wait */
 #include <readline/readline.h>
 #include <readline/history.h>
 
@@ -17,6 +19,17 @@ int main()
     {
         if (strcmp(buf,"quit")==0)
             break;
+        if (strcmp(buf,"pwd")==0) {
+            /*Spawn a child to run the program.*/
+            pid_t pid = fork();
+            if (pid==0) { /* child process */
+                execv("/bin/pwd",NULL);
+                exit(127); /* only if execv fails */
+            }
+            else { /* pid!=0; parent process */
+                waitpid(pid,0,0); /* wait for child to exit */
+            }
+        }
 
         printf("Você digitou o comando %s\n",buf);
 
@@ -27,6 +40,7 @@ int main()
     }
 
     free(buf);
+
 
     return 0;
 }
